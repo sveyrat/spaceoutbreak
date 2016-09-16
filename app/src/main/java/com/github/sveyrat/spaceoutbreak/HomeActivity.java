5ceb832f51038fe69ebf33656ff15f4da0af5b6f
sveyrat <sebastien.veyrat@gmail.com>
2016-09-16 17:35:40 +0200

    First version of the player input view

--- app/src/main/java/com/github/sveyrat/spaceoutbreak/HomeActivity.java
+++ app/src/main/java/com/github/sveyrat/spaceoutbreak/HomeActivity.java
@@ -1,7 +1,9 @@
 package com.github.sveyrat.spaceoutbreak;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void newGame(View view) {
+        Intent newGameIntent = new Intent(this, NewGameInputPlayerActivity.class);
+        startActivity(newGameIntent);
     }
 }
---
+++ app/src/main/java/com/github/sveyrat/spaceoutbreak/NewGameInputPlayerActivity.java
@@ -0,0 +1,36 @@
+package com.github.sveyrat.spaceoutbreak;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NewGameInputPlayerActivity extends AppCompatActivity {
+
+    private List<String> players = new ArrayList<>();
+    ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_game_input_player);
+        adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1,
+                players);
+        ListView listView = (ListView) findViewById(R.id.new_game_input_player_player_list);
+        listView.setAdapter(adapter);
+    }
+
+    public void addPlayer(View view) {
+        // TODO here : handle already existing player name and empty input cases
+        EditText playerNameEditText = (EditText) findViewById(R.id.new_game_input_player_player_name);
+        String playerName = playerNameEditText.getText().toString();
+        adapter.add(playerName);
+        playerNameEditText.setText("");
+    }
+}
