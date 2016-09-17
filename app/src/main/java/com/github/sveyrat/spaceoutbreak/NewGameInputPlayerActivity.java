791ceff54888207340a85631c741113aeffcf35d
romain <romain.dpz@gmail.com>
2016-09-17 15:54:16 +0200

    Check empty input or existing one when adding player

--- app/src/main/java/com/github/sveyrat/spaceoutbreak/NewGameInputPlayerActivity.java
+++ app/src/main/java/com/github/sveyrat/spaceoutbreak/NewGameInputPlayerActivity.java
@@ -4,12 +4,17 @@ import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.github.sveyrat.spaceoutbreak.util.StringUtil;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,33 +37,35 @@ public class NewGameInputPlayerActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> a, View v, int position, long id) {
                 final String itemName = ((TextView)v).getText().toString();
                 AlertDialog.Builder adb = new AlertDialog.Builder(NewGameInputPlayerActivity.this);
-                //adb.setTitle("Supprimer ?");
-                adb.setMessage("Supprimer " + itemName + "?");
-                final int positionToRemove = position;
-                adb.setNegativeButton("Non", null);
-                adb.setPositiveButton("Oui", new AlertDialog.OnClickListener() {
+                String message = String.format(getResources().getString(R.string.new_game_player_input_delete_message), itemName);
+                adb.setMessage(message);
+                adb.setNegativeButton(getResources().getString(R.string.new_game_player_input_no), null);
+                adb.setPositiveButton(getResources().getString(R.string.new_game_player_input_yes), new AlertDialog.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
-                        //MyDataObject.remove(positionToRemove); // delete from database
+                        //MyDataObject.remove(position); // delete from database
                         //adapter.notifyDataSetChanged();  //necessary if using datastore
                         adapter.remove(itemName);
-
                     }
                 });
                 adb.show();
             }
         });
-
-
-
-
-
     }
 
     public void addPlayer(View view) {
         // TODO here : handle already existing player name and empty input cases
         EditText playerNameEditText = (EditText) findViewById(R.id.new_game_input_player_player_name);
         String playerName = playerNameEditText.getText().toString();
-        adapter.add(playerName);
+        if(!playerName.isEmpty()) {
+            if(!StringUtil.containsIgnoreCase(players,playerName)) {
+                adapter.add(playerName);
+            }
+            else{
+                Toast toast = Toast.makeText(NewGameInputPlayerActivity.this, getResources().getString(R.string.new_game_player_input_same_player_error), Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                toast.show();
+            }
+        }
         playerNameEditText.setText("");
     }
 
