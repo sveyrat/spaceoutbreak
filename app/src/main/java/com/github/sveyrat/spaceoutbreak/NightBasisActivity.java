1dc159b5f7549a1688f9fb972607f8dc2cbfd79c
Romain <romain@stimshop.com>
2016-09-22 22:10:16 +0200

    Add night basis activity

---
+++ app/src/main/java/com/github/sveyrat/spaceoutbreak/NightBasisActivity.java
@@ -0,0 +1,22 @@
+package com.github.sveyrat.spaceoutbreak;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.github.sveyrat.spaceoutbreak.dao.RepositoryManager;
+
+/**
+ * Created by Rom on 22/09/2016.
+ */
+
+public class NightBasisActivity extends AppCompatActivity{
+
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            RepositoryManager.init(this);
+
+            setContentView(R.layout.activity_new_game_input_player);
+
+        }
+}
