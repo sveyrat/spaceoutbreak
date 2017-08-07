dc5454878b0e496c6dfa11aa06a02412e0786bd9
Romain <romain.dpz@gmail.com>
2017-08-07 18:20:16 +0200

    Created Auopsy activity

--- app/src/main/java/com/github/sveyrat/spaceoutbreak/AutopsyActivity.java
+++
@@ -1,8 +0,0 @@
-package com.github.sveyrat.spaceoutbreak;
-
-/**
- * Created by Rom on 02/08/2017.
- */
-
-public class AutopsyActivity {
-}
---
+++ app/src/main/java/com/github/sveyrat/spaceoutbreak/display/activity/AutopsyActivity.java
@@ -0,0 +1,11 @@
+package com.github.sveyrat.spaceoutbreak.display.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Rom on 02/08/2017.
+ */
+
+public class AutopsyActivity extends AppCompatActivity {
+
+}
